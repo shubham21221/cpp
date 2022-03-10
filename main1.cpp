f4fc9a46f67d16fc0b7b2450be4297a7f288adcf
shubh <shubhamganeshsonawane@gmail.com>
2022-03-10 17:36:10 +0530

    #2

---
+++ main1.cpp
@@ -0,0 +1,9 @@
+#include <iostream.h>//include header file
+using namespace std;//include the std;
+
+int main()//starting the program
+{
+    cout << "Hello world" << endl;//print the Hello world 
+    
+    return 0;//return funtion
+}
